7395a6e5348ac34489760daf9e0f6f8cc8580b18
lilyshiuan <91708853+lilyshiuan@users.noreply.github.com>
2022-02-09 00:46:23 -0800

    Update NamePicker.js

--- chatter/src/NamePicker.js
+++ chatter/src/NamePicker.js
@@ -1,39 +1,39 @@
-import React from "react";
 import { useState } from "react";
+import { FiEdit } from "react-icons/fi";
 
-
+// the component declaration
+// by convention, we name it with a capital first letter
 function NamePicker(props) {
-    const [editName, setEditName] = useState(false);
-    const [name, setName] = useState("");
+  // this is the state of the name itself
+  const [name, setName] = useState("");
+  // this is a toggle between showing and hiding the input
+  const [showInput, setShowInput] = useState(false);
 
-
-  function send() {
-    props.sendMessage(name);
-    setName("");
-  }
-  
-  function onKeyPress(e) {
-    if (e.key === "Enter") {
-      send();
-    }
+  // you clicked ok!
+  function ok() {
+    props.setUsername(name);
+    setShowInput(false);
   }
 
+  if (showInput) {
+    return (
+      <div className="name-picker">
+        <input
+          className="name-picker-input"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <button className="name-picker-button" onClick={ok}>
+          OK
+        </button>
+      </div>
+    );
+  }
   return (
-    <footer className="footer">
-      <button onClick={props.showCamera}
-        style={{left:10, right:'auto'}}>
-        <FiCamera style={{height:15, width:15}} />
-      </button>
-      <input
-        className="text-input"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyPress={onKeyPress}
-      />
-      <button className="send" onClick={send}>
-        <FiSend />
-      </button>
-    </footer>
+    <div className="name-picker">
+      <span className="name-picker-name">{name || "Set Username:"}</span>
+      <FiEdit size="24" onClick={() => setShowInput(true)} />
+    </div>
   );
 }
 
